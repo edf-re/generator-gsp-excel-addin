46c0f1ca42a898d712509b6feedf27cad7c0895b
Chris Stead <cmstead@gmail.com>
2021-10-07 10:51:56 -0700

    F Moving files for cleaner copy

--- generators/app/index.js
+++ generators/app/index.js
@@ -18,7 +18,7 @@ module.exports = class extends Generator {
       {
         type: 'input',
         name: 'projectFullName',
-        message: 'Full name of this project (do not include "addin")',
+        message: 'Name of this project (do not include "Addin")',
         validate: (name) => name.trim().length > 0
       }
     ];
@@ -26,17 +26,32 @@ module.exports = class extends Generator {
     return this.prompt(prompts)
       .then(props => {
         this.props = props;
-        this.props.projectName = props.projectFullName.replace(/\s+/gi, '-').toLowerCase();
-        this.env.cwd = `${this.props.projectName}-addin`
+        this.props.projectSlug = props.projectFullName.replace(/\s+/gi, '-').toLowerCase();
+        this.props.projectName = props.projectFullName
+          .trim()
+          .split(/\s+/gi, '-')
+          .map((token, index) => {
+            const firstChar = token[0];
+            return index > 0
+              ? `${firstChar.toUpperCase()}${token.slice(1)}`
+              : token;
+          })
+          .join('');
+
+        this.env.cwd = `${this.props.projectSlug}-addin`
       });
   }
 
   writing() {
     this.fs.copy(
-      this.templatePath('dummyfile.txt'),
-      this.destinationPath('dummyfile.txt')
+      this.templatePath(),
+      this.destinationPath(),
+      this.props
     );
   }
 
-  install() {}
+  install() {
+    process.chdir(this.env.cwd);
+    this.spawnCommand('npm', ['install']);
+  }
 };
